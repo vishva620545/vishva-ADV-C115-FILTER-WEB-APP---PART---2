9d9b40424146f3b3e8f76717f950340f4e15d948
vishva620545 <78867324+vishva620545@users.noreply.github.com>
2021-05-12 21:24:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(500,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is Initialized');
+}
+
+function draw()
+{
+ image(video,0,0,500,500);
+}
+
+function take_snapshot()
+{
+    save('class114.png');
+}
+
+function gotPoses(results)
+{
+ if(results.length>0)
+ {
+     console.log(results);
+     console.log("nose x="+results[0].pose.nose.x);
+     console.log("nose y="+results[0].pose.nose.y);
+ }
+}
